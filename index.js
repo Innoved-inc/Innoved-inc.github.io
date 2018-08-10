f1115ff4ae4f38632fc596342892bb2a29d15632
Jordan Mavrogeorge <jordomav@gmail.com>
2018-08-10 00:40:31 -0700

    done

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ function getRepos() {
     return fetch('https://api.github.com/users/Innoved-inc/repos').then(data => { return data.json() }).then(repos => {
         let sites = document.getElementById("sites");
         repos.forEach(repo => {
+            if (repo.name === 'Innoved-inc.github.io') { return; }
             let site = document.createElement("a");
             site.setAttribute('href', 'https://innoved-inc.github.io/' + repo.name);
             site.setAttribute('target', '_blank');
-            site.setAttribute('class', 'list-group-item list-group-item-action');
+            site.setAttribute('class', 'list-group-item list-group-item-action innoved-list');
             site.appendChild(document.createTextNode(repo.name));
             sites.appendChild(site);
         });
